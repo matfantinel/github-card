088668d8fafe32fa8c99f2d72007ce36b0ded94c
Matheus Fantinel <matfantinel@gmail.com>
2019-11-27 00:01:29 -0300

    Repo pagination

--- src/components/github-card/github-card.tsx
+++ src/components/github-card/github-card.tsx
@@ -1,10 +1,10 @@
-import { Component, Host, h, Prop } from '@stencil/core';
-import { GithubUserData } from '../../models/user-data';
-import { GithubRepoData } from '../../models/repo-data';
+import { Component, Host, h, Prop } from "@stencil/core";
+import { GithubUserData } from "../../models/user-data";
+import { GithubRepoData } from "../../models/repo-data";
 
 @Component({
-  tag: 'github-card',
-  styleUrl: 'github-card.scss',
+  tag: "github-card",
+  styleUrl: "github-card.scss",
   shadow: true
 })
 export class GithubCard {
@@ -12,11 +12,14 @@ export class GithubCard {
 
   userData: GithubUserData;
   repoData: GithubRepoData[];
+  starsTotalCount: number;
   rateExceeded: boolean;
+
+  private _maxPerPage: number = 100;
 
   componentWillLoad() {
     if (!this.username) {
-      console.error('No username was provided');
+      console.error("No username was provided");
       return;
     }
 
@@ -29,84 +32,90 @@ export class GithubCard {
     return request
       .then(response => response.json() as Promise<GithubUserData>)
       .then(async data => {
-        if ((data as any).message.indexOf('API rate limit exceeded')) {
+        if (
+          (data as any).message &&
+          (data as any).message.indexOf("API rate limit exceeded")
+        ) {
           this.rateExceeded = true;
         } else {
           this.userData = data;
-          await this.fetchUserRepos(this.userData.repos_url);
+          this.repoData = await this.fetchUserRepos(this.userData.repos_url);
+          if (this.repoData) {
+            this.starsTotalCount = this.repoData.reduce(
+              (sum, current) => sum + current.stargazers_count,
+              0
+            );
+          }
         }
       });
   }
 
-  fetchUserRepos(reposUrl: string) {
-    return new Promise<void>(resolve => {
-      debugger
+  fetchUserRepos(reposUrl: string, page: number = 1) {
+    return new Promise<Array<GithubRepoData>>(resolve => {
       let result = new Array<GithubRepoData>();
-      let maxPerPage = 100;
-      let keepFetching = true;
-      let page = 1;
 
-      // while (keepFetching) {
-        let request = fetch(`${reposUrl}?sort=stars&order=desc&per_page=${maxPerPage}&page=${page}`);
+      let request = fetch(
+        `${reposUrl}?sort=stars&order=desc&per_page=${this._maxPerPage}&page=${page}`
+      );
 
-        request
-          .then(response => response.json() as Promise<GithubRepoData[]>)
-          .then(data => {
-            result.concat(data);
+      request
+        .then(response => response.json() as Promise<GithubRepoData[]>)
+        .then(async data => {
+          result = result.concat(data);
 
-            if (data.length < maxPerPage) {
-              keepFetching = false;
-              resolve();
-            } else {
-              page++;
-            }
-          });
-      // }
+          if (data.length >= this._maxPerPage) {
+            page++;
+            result.concat(await this.fetchUserRepos(reposUrl, page));
+          }
+
+          resolve(result);
+        });
     });
-  }
-
-  getStarsTotalCount() {
-    return this.repoData.reduce((sum, current) => sum + current.stargazers_count, 0);
   }
 
   render() {
     return (
-      <div class='card'>
+      <div class="card">
         {this.userData.avatar_url ? (
-          <div class='card-avatar'>
+          <div class="card-avatar">
             <img src={this.userData.avatar_url}></img>
           </div>
         ) : (
-          ''
+          ""
         )}
 
-        <div class='card-details'>
-          <div class='name'>
+        <div class="card-details">
+          <div class="name">
             {this.userData.name}
-            {this.userData.company ? <span class='works-for'>&nbsp;-&nbsp;{this.userData.company}</span> : ''}
+            {this.userData.company ? (
+              <span class="works-for">
+                &nbsp;-&nbsp;{this.userData.company}
+              </span>
+            ) : (
+              ""
+            )}
           </div>
-          <div class='detail home-location'>{this.userData.location}</div>
-
-          <div class='card-about'>
-            <div class='item'>
-              <span class='value'>{this.getStarsTotalCount()}</span>
-              <span class='label'>Stars</span>
+          <div class="detail home-location">{this.userData.location}</div>
+          <div class="card-about">
+            <div class="item">
+              <span class="value">{this.starsTotalCount}</span>
+              <span class="label">Stars</span>
             </div>
-            <div class='item'>
-              <span class='value'>{this.userData.public_repos}</span>
-              <span class='label'>Repos</span>
+            <div class="item">
+              <span class="value">{this.userData.public_repos}</span>
+              <span class="label">Repos</span>
             </div>
-            <div class='item'>
-              <span class='value'>{this.userData.followers}</span>
-              <span class='label'>Followers</span>
+            <div class="item">
+              <span class="value">{this.userData.followers}</span>
+              <span class="label">Followers</span>
             </div>
           </div>
           {this.userData.bio ? (
-            <div class='skills'>
-              <span class='value'>{this.userData.bio}</span>
+            <div class="skills">
+              <span class="value">{this.userData.bio}</span>
             </div>
           ) : (
-            ''
+            ""
           )}
         </div>
       </div>
